f3419c7d9f6dca28135f7d18447ff7ad7d0f8be6
dianaparr <dianaparra.9611@gmail.com>
2021-10-11 12:50:38 -0500

    Upload new set up to the table history, add id and date

--- table_history.sql
+++ table_history.sql
@@ -1,6 +1,7 @@
 -- Create receiver table in remittances_db
 CREATE TABLE IF NOT EXISTS history (
+    id serial NOT NULL PRIMARY KEY,
+    date DATE DEFAULT (CURRENT_DATE),
     phone VARCHAR(20) NOT NULL,
-    balance VARCHAR(20),
-    date VARCHAR(20)
+    balance VARCHAR(20)
 );
