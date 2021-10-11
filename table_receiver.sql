ada10e8a31e6b918db1b26ef70f2c2bdbb0dcc28
dianaparr <dianaparra.9611@gmail.com>
2021-10-11 15:13:55 -0500

    Add parameter identify

--- table_receiver.sql
+++ table_receiver.sql
@@ -1,5 +1,6 @@
 -- Create receiver table in remittances_db
 CREATE TABLE IF NOT EXISTS receiver (
+    id serial NOT NULL PRIMARY KEY,
     name VARCHAR(120) NOT NULL,
     phone VARCHAR(10) NOT NULL,
     cash VARCHAR(100) NOT NULL
