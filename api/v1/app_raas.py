5cd7c35b548e0e700c179a0edd58800b0f879b11
victor <vicuartas230@gmail.com>
2021-10-14 13:11:50 -0500

    Change all style

--- api/v1/app_raas.py
+++ api/v1/app_raas.py
@@ -2,9 +2,13 @@
 
 from api.v1.views import app_views
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
