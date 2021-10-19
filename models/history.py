f5eff8ab85c4ee92ff009b7e54e88e2fe38bd651
dianaparr <dianaparra.9611@gmail.com>
2021-10-19 12:33:46 -0500

    Update datetime

--- models/history.py
+++ models/history.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ history module """
-import datetime
-from sqlalchemy import Column, Integer, String, Date
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,7 +12,7 @@ class History(Base):
     __tablename__ = 'history'
 
     id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
-    date = Column(Date, nullable=False)
+    date = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
     phone = Column(String(40), nullable=False)
     balance = Column(String(100), nullable=False)
 
@@ -22,9 +22,9 @@ class History(Base):
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-            self.date = datetime.datetime.now()
+            self.date = datetime.utcnow()
         else:
-            self.date = datetime.datetime.now()
+            self.date = datetime.utcnow()
 
     def print_dict(self):
         """ to print the dictionary """
