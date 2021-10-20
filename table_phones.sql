7475c5dbe24e4dc516b34b7a924fd7cd5c602a39
Brian Zapata <dzapata397@gmail.com>
2021-10-19 20:41:44 -0500

    The script was changed

--- table_phones.sql
+++ table_phones.sql
@@ -3,9 +3,5 @@ CREATE TABLE IF NOT EXISTS phones (
     id serial NOT NULL,
     phone VARCHAR(40) NOT NULL,
     phone_desencrypt VARCHAR(40) NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_phone
-        FOREIGN KEY (phone)
-            REFERENCES receiver(phone)
-            ON DELETE CASCADE
+    PRIMARY KEY (id)
 );
