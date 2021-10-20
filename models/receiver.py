6f4e7a7e75b1f2d3abfe22f5573997d6840bfaed
Brian Zapata <dzapata397@gmail.com>
2021-10-19 21:56:53 -0500

    The relacionship was removed

--- models/receiver.py
+++ models/receiver.py
@@ -3,7 +3,6 @@
 from models.phones import Phones
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
