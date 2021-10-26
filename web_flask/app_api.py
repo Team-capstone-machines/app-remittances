b32352f67bd9965d0651bb0d423430fd25f332fe
dianaparr <dianaparra.9611@gmail.com>
2021-10-26 10:55:30 -0500

    Add jinja

--- web_flask/app_api.py
+++ web_flask/app_api.py
@@ -55,7 +55,7 @@ def sender():
                     data=dict_put, headers=headers)
                 return render_template('sended.html')
         else:
-            return render_template('bad_name.html')
+            return render_template('bad_name.html', sender=sender)
 
 
 @app.route('/receiver', strict_slashes=False)
