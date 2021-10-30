5a25cecb047ffbf7f832cc4f7fff2319281e6b32
Brian Zapata <dzapata397@gmail.com>
2021-10-30 14:26:36 -0500

    Empty test branch

--- table_receiver.sql
+++
@@ -1,8 +0,0 @@
--- Create receiver table in remittances_db
-CREATE TABLE IF NOT EXISTS receiver (
-    id serial NOT NULL,
-    name VARCHAR(120) NOT NULL,
-    phone VARCHAR(40) NOT NULL,
-    cash VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-);
